ade985f0439517823613ad4a0303cb194d4b2fc1
Nouha11 <52921028+Nouha11@users.noreply.github.com>
2021-10-22 06:55:08 +0100

    Update rate.js

--- src/Components/rate.js
+++ src/Components/rate.js
@@ -1,18 +1,21 @@
-import React, {useState} from 'react'
+import React,{useState} from 'react'
 
 export default function Rate({setStars}) {
     const [color, setColor] = useState("")
-    colorChange = () => {
-        setColor("gold")
+   const colorChange = () => {
+        if(color === "white" || color === ""){
+            setColor("gold");
+        }
+        else {setColor("white")}
     }
 
     return (
         <div className="Rstars">
-            <i class="fas fa-star" style={{color: color}} id={Math.random()*1000} onClick={() => {setStars(1) , colorChange} }></i>
-            <i class="fas fa-star" id={Math.random()*1000} onClick={() => setStars(2)}> </i>
-            <i class="fas fa-star" id={Math.random()*1000} onClick={() => setStars(3)}></i>
-            <i class="fas fa-star" id={Math.random()*1000} onClick={() => setStars(4)}></i>
-            <i class="fas fa-star" id={Math.random()*1000} onClick={() => setStars(5)}></i>
+            <i class="fas fa-star" style={{color: color}} id={Math.random()*1000} onClick={() => {setStars(1) , colorChange()} }></i>
+            <i class="fas fa-star" style={{color: color}} id={Math.random()*1000} onClick={() => {setStars(2) , colorChange()}}> </i>
+            <i class="fas fa-star" style={{color: color}} id={Math.random()*1000} onClick={() => {setStars(3) , colorChange()}}></i>
+            <i class="fas fa-star" style={{color: color}} id={Math.random()*1000} onClick={() => {setStars(4) , colorChange()}}></i>
+            <i class="fas fa-star" style={{color: color}} id={Math.random()*1000} onClick={() => {setStars(5) , colorChange()}}></i>
 
         </div>
     )
