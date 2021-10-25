8f7437279de16d92625c7fb248c9d246d05e9363
Nouha11 <52921028+Nouha11@users.noreply.github.com>
2021-10-25 11:18:17 +0100

    Add files via upload

---
+++ src/Components/navbar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Navbar,Container,Nav } from "react-bootstrap";
+import Rate from "./rate";
+
+
+function Head ({setSearch,setStars, stars}) {
+    const getInput = (e) => {
+        setSearch(e.target.value)
+    }
+
+    return (
+        <div className="Box">
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                <Navbar.Brand href="#home">Movie App</Navbar.Brand>
+                <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <div className="searchContainer">
+                <input placeholder="Search for MOVIES !" className="searchBar" onChange={getInput}></input>
+                </div>
+                <Nav className="searchStars">
+                <h4>
+                    <Rate 
+                    stars={stars}
+                    setStars={setStars} />
+                </h4>
+                </Nav>
+                </Nav>
+                </Container>
+            </Navbar>
+        </div>
+    )
+}
+
+export default Head;
